e12bc2061fd50ac4269aa08c018d2d658bfcb6d5
yoonseok <user@DESKTOP-DIAKH25.localdomain>
2022-04-17 02:28:23 +0900

    Component (HTML Form) 작성 완료

---
+++ src/view/exchangeRate/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExchangeInfo from '../../component/index';
+
+const ExchangeRatePage = () => {
+
+    return <>
+        <ExchangeInfo />
+    </>
+};
+
+
+export default ExchangeRatePage;
