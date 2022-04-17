a6444634140a0deeb635d0da887590c60239c23c
YoonSeok Lee <52606813+goodgood619@users.noreply.github.com>
2022-04-17 23:54:30 +0900

    component 및 page 및 util 구현

--- src/component/index.jsx
+++ src/component/index.jsx
@@ -1,33 +1,57 @@
-const ExchangeInfo = ({exchangeMoney, exchangeDefinition}) => {
+import React,{ useState } from "react";
+import {MakeLowerCase,MakeUpperCase} from '../util/index';
+
+const ExchangeInfo = ({getExchangeRate, exchangeValue,postReceptionMoney,receivedReceptionMoney}) => {
+    const handleSelect = (e) => {
+        getExchangeRate('usd',e.target.value);
+        if (e.target.value == "") {
+            setToSend(null);
+        }
+        else {
+            setToSend(e.target.value);
+        }
+    }
+
+    const handleReceptionMoney = (e) => {
+        setReceptionMoney(e.target.value);
+    }
+
+    const submitReceptionMoney = (e) => {
+        e.preventDefault();
+        postReceptionMoney('usd',toSend,receptionMoney);
+    }
+
+    const [toSend, setToSend] = useState("");
+    const [receptionMoney,setReceptionMoney] = useState('');
 
     return (
         <>
-                <h1>환율 계산</h1>
+<h1>환율 계산</h1>
 <div>
     <p>
         <strong> 송금 국가 : 미국(USD) </strong></p>
     <p>
-        <strong>수취 국가 : <select name="job">
-                                <option value="">환율선택</option>
-                                <option value="Krw">한국(KRW)</option>
-                                <option value="Jpy">일본(JPY)</option>
-                                <option value="Php">필리핀(PHP)</option>
+        <strong>수취 국가 : <select name="job" onChange={handleSelect} value={toSend}>
+                                    <option value="">환율 선택</option>
+                                    <option value="Krw">한국(KRW)</option>
+                                    <option value="Jpy">일본(JPY)</option>
+                                    <option value="Php">필리핀(PHP)</option>
                             </select>
         </strong>
     </p>
     <p>
-        <b>환율</b> : <b>{exchangeMoney} {exchangeDefinition}</b>
+        <b>환율</b> : <b>{exchangeValue}</b> {exchangeValue && <b>{MakeUpperCase(toSend)}/{MakeUpperCase('usd')}</b>}
     </p>
     <form>
         <label>
           송금액:
-             <input type="text" value="" />
+             <input type="value" value={receptionMoney} onChange={handleReceptionMoney} USD/>
         </label>
   	    <br/>
-            <input type="submit" value="Submit" />
+            <input type="submit" value="Submit" onClick={submitReceptionMoney}/>
     </form>
     <p>
-        수취 금액은 얼마입니다
+        수취 금액은 <b>{receivedReceptionMoney}</b> {receivedReceptionMoney && <b>{MakeUpperCase(toSend)}</b>}입니다.
     </p>
 </div>                       
     </> 
